8abf787c0923702977c9ee29ae826afb3082d91b
mahmedmushtaq <mahmedmushtaq296@gmail.com>
2023-02-21 14:37:34 +0500

    setup basic chrome extension template in reactjs

---
+++ src/popup/Popup.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const Popup = () => (
+  <div>
+    <h1 className="text-5xl text-green-500">Hello world</h1>
+    <p>Lorem ipsum generator</p>
+  </div>
+);
+
+export default Popup;
